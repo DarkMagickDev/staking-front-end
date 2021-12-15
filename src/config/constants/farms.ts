b8d124031776f840be05cdedd14bcdfe901109b0
DarkMagick <90057502+darkmagick@users.noreply.github.com>
2021-12-15 10:50:59 -0500

    update

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -50,23 +50,6 @@ const farms: FarmConfig[] = [
     quoteTokenSymbol: QuoteToken.BUSD,
     quoteTokenAdresses: contracts.busd,
   },
-    {
-    pid: 3,
-    risk: 3,
-    lpSymbol: 'DMGK',
-    lpAddresses: {
-      97: '',
-      56: '0x6F89660fEf8B6a03a41E5F3d0306645BE2469eFE',
-    },
-    isTokenOnly: true,
-    tokenSymbol: 'DMGK',
-    tokenAddresses: {
-      97: '',
-      56: '0x6F89660fEf8B6a03a41E5F3d0306645BE2469eFE',
-    },
-    quoteTokenSymbol: QuoteToken.BUSD,
-    quoteTokenAdresses: contracts.busd,
-  },
   // {
   //   pid: 3,
   //   risk: 1,
